c1ef87489b78eaef904efeeb3d9d451a8211143b
Sam Knight <samknight@Sams-Work-MacBook-Pro.local>
2020-01-25 16:55:16 +0000

    schedule

--- app/models/reduction.rb
+++ app/models/reduction.rb
@@ -11,6 +11,24 @@ class Reduction < ApplicationRecord
     (pills - (pills * (0.1 * week))).round
   end
 
+  # [2, 2, 2, 1]
   def schedule(week)
+    pill_per_day = pill_reduction(week) #9
+    individual_dose = (pill_per_day / medication.frequency).floor #1
+    puts individual_dose
+    puts "------"
+    array = []
+    medication.frequency.times { |a| array << individual_dose } #[1, 1, 1, 1]
+    puts array.inspect
+    puts "-----"
+    remaining = pill_per_day - array.sum #3
+    puts remaining
+    puts "-----"
+    remaining.times do |r|
+      array[r] = array[r]+1
+    end
+    puts array
+    puts "========="
+    return array
   end
 end
