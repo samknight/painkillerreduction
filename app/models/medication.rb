9611b69f60fc80f8c48228d58d6d945126a7df34
Sam Knight <samknight@Sams-Work-MacBook-Pro.local>
2020-01-26 11:20:39 +0000

    Change validation

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -3,8 +3,8 @@ class Medication < ApplicationRecord
   belongs_to :drug
   has_many :reduction
 
-  validates :dose, presence: true, numericality: { only_integer: true, greater_than: 1 }
-  validates :frequency, presence: true, numericality: { only_integer: true, greater_than: 1 }
+  validates :dose, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :frequency, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
   def morphine_equivalent
     drug.dosage.to_i * drug.morphine * dose * frequency
